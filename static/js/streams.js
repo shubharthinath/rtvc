cc801fa4521118b37be69bb6b06c69919efc3cf2
Shubharthi <shubharthi24nath@gmail.com>
2023-03-31 03:37:19 +0530

    set up room and agora

---
+++ static/js/streams.js
@@ -0,0 +1,3 @@
+const APP_ID = "a24681963b024ac8b77ea8739f5f1dcf";
+const CHANNEL = "main";
+const TOKEN = "007eJxTYGhfEXFu8+2ry/9KnRRsSBKO5GfR8ar982jJggkOTav9vdcqMCQamZhZGFqaGScZGJkkJlskmZunJlqYG1ummaYZpiSnqbKopTQEMjJMXVTNwsgAgSA+C0NuYmYeAwMAUtke0Q==";
