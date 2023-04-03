5c84639f54b6d7ec0a3ccc3f906b947f8645e578
Shubharthi <shubharthi24nath@gmail.com>
2023-04-03 22:18:59 +0530

    set up video display

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,3 +1,29 @@
-const APP_ID = "a24681963b024ac8b77ea8739f5f1dcf";
-const CHANNEL = "main";
-const TOKEN = "007eJxTYGhfEXFu8+2ry/9KnRRsSBKO5GfR8ar982jJggkOTav9vdcqMCQamZhZGFqaGScZGJkkJlskmZunJlqYG1ummaYZpiSnqbKopTQEMjJMXVTNwsgAgSA+C0NuYmYeAwMAUtke0Q==";
+const APP_ID = "a24681963b024ac8b77ea8739f5f1dcf"
+const CHANNEL = "main"
+const TOKEN =
+  "007eJxTYPCpbQg7kbG1jW3+r2B+Kysmi4P61y+1ceZbr8qYxn/XQFyBIdHIxMzC0NLMOMnAyCQx2SLJ3Dw10cLc2DLNNM0wJTnt4DutlIZARoblhasYGRkgEMRnYchNzMxjYAAApogdxA==";
+let UID
+
+const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" })
+
+let localTracks = [];
+let remoteUsers = {};
+
+let joinAndDisplayLocalStream = async () => {
+  UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+
+  let player = `<div class="video-container" id="user-container-${UID}">
+                        <div class="username-wrapper"><span class="user-name">My Name</span></div>
+                        <div class="video-player" id="user-${UID}"></div>
+                    </div>`
+  document
+    .getElementById("video-streams")
+    .insertAdjacentHTML("beforeend", player)
+
+  localTracks[1].play(`user-${UID}`)
+
+  await client.publish([localTracks[0], localTracks[1]])
+}
+
+joinAndDisplayLocalStream();
